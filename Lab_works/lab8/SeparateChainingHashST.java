9417020a7a727748688cf6c6f91c788f5e61a3fd
Gyeltshenwang <khotshawangdi@gmail.com>
2020-12-03 22:08:44 +0600

    lab8

---
+++ Lab_works/lab8/SeparateChainingHashST.java
@@ -0,0 +1,175 @@
+import java.util.*;
+public class SeparateChainingHashST<Key, Value> {
+    private static final int INIT_CAPACITY = 4;
+
+    private int n;                                // number of key-value pairs
+    private int m;                                // hash table size
+    private SequentialSearchST<Key, Value>[] st;  // array of linked-list symbol tables
+    /**
+     * Initializes an empty symbol table.
+     */
+    public SeparateChainingHashST() {
+        this(INIT_CAPACITY);
+    } 
+
+    /**
+     * Initializes an empty symbol table with {@code m} chains.
+     * @param m the initial number of chains
+     */
+    public SeparateChainingHashST(int m) {
+        this.m = m;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
+        for (int i = 0; i < m; i++)
+            st[i] = new SequentialSearchST<Key, Value>();
+    } 
+
+    // resize the hash table to have the given number of chains,
+    // rehashing all of the keys
+    private void resize(int chains) {
+        SeparateChainingHashST<Key, Value> temp = new SeparateChainingHashST<Key, Value>(chains);
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys()) {
+                temp.put(key, st[i].get(key));
+            }
+        }
+        this.m  = temp.m;
+        this.n  = temp.n;
+        this.st = temp.st;
+    }
+
+    // hash function for keys - returns value between 0 and m-1
+    private int hashTextbook(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+    private int hash(Key key) {
+        int h = key.hashCode();
+        h ^= (h >>> 20) ^ (h >>> 12) ^ (h >>> 7) ^ (h >>> 4);
+        return h & (m-1);
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+       return n;
+    } 
+
+    /**
+     * Returns true if this symbol table is empty.
+     *
+     * @return {@code true} if this symbol table is empty;
+     *         {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size()==0;
+    }
+
+    /**
+     * Returns true if this symbol table contains the specified key.
+     *
+     * @param  key the key
+     * @return {@code true} if this symbol table contains {@code key};
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    } 
+
+    /**
+     * Returns the value associated with the specified key in this symbol table.
+     *
+     * @param  key the key
+     * @return the value associated with {@code key} in the symbol table;
+     *         {@code null} if no such value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        int i = hash(key);
+        return st[i].get(key);    
+    } 
+
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting the old 
+     * value with the new value if the symbol table already contains the specified key.
+     * Deletes the specified key (and its associated value) from this symbol table
+     * if the specified value is {@code null}.
+     *
+     * @param  key the key
+     * @param  val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(Key key, Value val) {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        // double table size if average length of list >= 10
+        if (n >= 10*m) resize(2*m);
+
+        int i = hash(key);
+        if (!st[i].contains(key)) n++;
+        st[i].put(key, val);
+    } 
+
+    /**
+     * Removes the specified key and its associated value from this symbol table     
+     * (if the key is in this symbol table).    
+     *
+     * @param  key the key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+
+        int i = hash(key);
+        if (st[i].contains(key)) n--;
+        st[i].delete(key);
+
+        // halve table size if average length of list <= 2
+        if (m > INIT_CAPACITY && n <= 2*m) resize(m/2);
+    } 
+
+    // return keys in symbol table as an Iterable
+    public Iterable<Key> keys() {
+        ArrayList<Key> arr = new ArrayList<Key>();
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys())
+                arr.add(key);
+        }
+        return arr;
+    } 
+    // /**
+    //  * Unit tests the {@code SeparateChainingHashST} data type.
+    //  *
+    //  * @param args the command-line arguments
+    //  */
+    public static void main(String[] args) { 
+        SeparateChainingHashST<String, Integer> st = new SeparateChainingHashST<String, Integer>();
+        
+        assert(st.isEmpty()==true);
+
+        st.put("gyeltshen",2);
+        st.put("sonam",1);
+        st.put("Karma",3);
+        st.put("jambay",1);
+
+        assert(st.isEmpty()==false);
+        assert(st.size()==3);
+
+        assert(st.contains("gyeltshen"));
+        //assert(st.get("Karma"));
+
+        System.out.print("All test  are passed");
+
+
+
+
+    }
+}
